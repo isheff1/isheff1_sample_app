04b21a22212eb69d2659a65b47a55936c22ad2c0
Ian Shefferman <isheff1@umbc.edu>
2014-02-23 21:22:17 -0500

    Complete 6.5.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   has_secure_password
   validates :password, length: { minimum: 6 }
